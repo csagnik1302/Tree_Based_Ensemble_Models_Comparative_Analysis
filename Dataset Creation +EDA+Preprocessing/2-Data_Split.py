d46fda232e712bccd58db7f5ffac0411d689d99e
csagnik1302 <sagnikc2003@gmail.com>
2026-05-25 13:56:43 +0530

    Model Metric computation completed

--- Dataset Creation +EDA+Preprocessing/2-Data_Split.py
+++ Dataset Creation +EDA+Preprocessing/2-Data_Split.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder,StandardScaler
 
-data=pd.read_csv(r"D:\ML Project\Data.csv")
+data=pd.read_csv(r"D:\ML Project\Dataset Creation +EDA+Preprocessing\Data_Feature_Engineered.csv")
 
 data_Y1=data["class"]
 data_X=data.drop(columns=["class"])
@@ -19,13 +19,23 @@ data_Y=pd.Series(Y_Encoder.transform(data_Y1),name='class')   # Since transform 
 
 ############# Perform Train-Test-Validation Split #####################
 
-X_train1,X_test,Y_train1,Y_test=train_test_split(data_X,data_Y,test_size=0.1)
+X_train1,X_test,Y_train1,Y_test=train_test_split(data_X,data_Y,test_size=0.1,random_state=50)
 
-X_train,X_val,Y_train,Y_val=train_test_split(X_train1,Y_train1,test_size=0.11111)
+X_train,X_val,Y_train,Y_val=train_test_split(X_train1,Y_train1,test_size=0.11111,random_state=50)
 
 Y_train=pd.Series(Y_train)
 Y_val=pd.Series(Y_val)
 Y_test=pd.Series(Y_test)
+
+
+
+############ Normalize Feature Data Splits (To remove any feature dominance issues) ###################
+
+normalizer=StandardScaler().set_output(transform='pandas')
+
+X_train=normalizer.fit_transform(X_train)
+X_val=normalizer.transform(X_val)
+X_test=normalizer.transform(X_test)
 
 
 ############# Exporting Splits #########################################
---
+++ Dataset Creation +EDA+Preprocessing/4-Preprocessing.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+data=pd.read_csv(r"/home/Sagnik/as+project/Data.csv")
+
+print(data)
